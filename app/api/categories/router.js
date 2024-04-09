3a9ba6e0ecf120ba04fa7c3c0d7fe5dbe4ad9a7e
Dimas Nuryadin <dimasnuryadinn@gmail.com>
2024-04-09 22:13:05 +0700

    create deleteCategories API

--- app/api/categories/router.js
+++ app/api/categories/router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { isLogin } = require('../../middleware/auth');
-const { getAllCategories, createCategories, updateCategories } = require('./controller');
+const { getAllCategories, createCategories, updateCategories, deleteCategories } = require('./controller');
 
 router.get('/categories', isLogin, getAllCategories);
 router.post('/categories', isLogin, createCategories);
 router.put('/categories/:id', isLogin, updateCategories);
+router.delete('/categories/:id', isLogin, deleteCategories);
 
 module.exports = router;
