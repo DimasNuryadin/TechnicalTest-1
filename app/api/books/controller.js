1dae52918813b219ddcfa683773ae0ad2f9107da
Dimas Nuryadin <dimasnuryadinn@gmail.com>
2024-04-10 19:10:26 +0700

    create API updateBooks and deleteBooks

--- app/api/books/controller.js
+++ app/api/books/controller.js
@@ -59,5 +59,47 @@ module.exports = {
     } catch (err) {
       next(err)
     }
+  },
+  updateBooks: async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const user = req.user.id;
+      const { title, price, category, author, published, stock, image } = req.body;
+
+      const checkBooks = await Book.findOne({ where: { id } });
+
+      if (!checkBooks) {
+        return res.status(404).json({ message: 'Id book tidak ditemukan' })
+      }
+
+      const books = await checkBooks.update({
+        title,
+        price,
+        category,
+        author,
+        published,
+        stock,
+        image,
+        user,
+      })
+
+      res.status(201).json({ message: 'Success update book', data: books })
+
+    } catch (err) {
+      next(err)
+    }
+  },
+  deleteBooks: (req, res, next) => {
+    Book.findOne({
+      where: {
+        id: req.params.id,
+        user: req.user.id,
+      }
+    }).then((books) => {
+      if (books) {
+        books.destroy();
+        res.status(200).json({ message: 'Success delete book', data: books })
+      }
+    }).catch((err) => next(err))
   }
 }
--- app/api/books/router.js
+++ app/api/books/router.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { isLogin } = require('../../middleware/auth');
-const { getAllBooks, createBooks } = require('./controller');
+const { getAllBooks, createBooks, updateBooks, deleteBooks } = require('./controller');
 
 router.get('/books', isLogin, getAllBooks);
 router.post('/books', isLogin, createBooks);
-// router.put('/books/:id', isLogin, updateBooks);
-// router.delete('/books/:id', isLogin, deleteBooks);
+router.put('/books/:id', isLogin, updateBooks);
+router.delete('/books/:id', isLogin, deleteBooks);
 
 module.exports = router;
